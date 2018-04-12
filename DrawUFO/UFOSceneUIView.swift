181a52e0ea62819070c8b343be01ae146ba6114d
Modali,Naga Sravanthi <s528749@ch140036m.nwmissouri.edu>
2018-04-12 12:41:05 -0500

    added code to draw circles to see how the fill happens

--- DrawUFO/UFOSceneUIView.swift
+++ DrawUFO/UFOSceneUIView.swift
@@ -20,20 +20,28 @@ class UFOSceneUIView: UIView {
         ufoBezier().stroke()
         ufoBezier().fill()
         var path = ufoBezier()
-        var moveIt = CGAffineTransform(translationX: -100.0, y: 30.0)
-        path.apply(moveIt)
+//        var moveIt = CGAffineTransform(translationX: -100.0, y: 30.0)
+//        path.apply(moveIt)
         path.stroke()
     }
  
 
     func ufoBezier() -> UIBezierPath {
-        let bounds = CGRect(x: 200, y: 40, width: 40, height: 20)
+//        let bounds = CGRect(x: 200, y: 40, width: 40, height: 20)
+//        let path = UIBezierPath(ovalIn: bounds)
+//        var center = bounds.origin
+//        center.x = center.x + 20
+//        let dome = UIBezierPath(arcCenter: center, radius: 10.0, startAngle: .pi/10.0, endAngle: .pi - .pi/10, clockwise: false)
+        
+        let bounds = CGRect(x: 200, y: 40, width: 50, height: 50)
         let path = UIBezierPath(ovalIn: bounds)
-        var center = bounds.origin
-        center.x = center.x + 20
-        let dome = UIBezierPath(arcCenter: center, radius: 10.0, startAngle: .pi/10.0, endAngle: .pi - .pi/10, clockwise: false)
+        let bounds2 = CGRect(x: 225, y: 40, width: 50, height: 50)
+        let dome = UIBezierPath(ovalIn: bounds2)
+        let bounds3 = CGRect(x: 215, y: 60, width: 50, height: 50)
+        let dome2 = UIBezierPath(ovalIn: bounds3)
         
         path.append(dome)
+        path.append(dome2)
         return path
     }
 }
